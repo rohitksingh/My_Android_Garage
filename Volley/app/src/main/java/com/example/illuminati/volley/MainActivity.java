d6876f92499529b99f5fa3719b606ddf427b9ff0
rohitksingh <r4rohit002@gmail.com>
2016-08-30 11:22:52 -0700

    My first Volley Request (String data)

---
+++ Volley/app/src/main/java/com/example/illuminati/volley/MainActivity.java
@@ -0,0 +1,81 @@
+package com.example.illuminati.volley;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.w3c.dom.Text;
+
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RequestQueue requestQueue;
+    private TextView jsonText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        jsonText=(TextView)findViewById(R.id.JsonText);
+        requestQueue= Volley.newRequestQueue(this);
+        fetchStringJsonData();
+    }
+
+    public void fetchStringJsonData()
+    {
+        String url="http://httpbin.org/html";
+        StringRequest request=new StringRequest(Request.Method.GET, url, new Response.Listener<String>(){
+            @Override
+            public void onResponse(String response)
+            {
+                jsonText.setText(response);
+               // System.out.println(response);
+            }
+        },
+                new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        requestQueue.add(request);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
