3ece336dfbbd5c09537a9ef1bc6b968c3ae5ae92
rohitksingh <r4rohit002@gmail.com>
2016-08-31 11:15:34 -0700

    JSONArray data fetched JSONObjects inside array

--- Volley/app/src/main/java/com/example/illuminati/volley/MainActivity.java
+++ Volley/app/src/main/java/com/example/illuminati/volley/MainActivity.java
@@ -15,9 +15,14 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.w3c.dom.Text;
 
 import java.util.Objects;
@@ -34,12 +39,13 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         jsonText=(TextView)findViewById(R.id.JsonText);
         requestQueue= Volley.newRequestQueue(this);
-        fetchStringJsonData();
+        //fetchStringJsonData();
+        fetchArrayJsonData();
     }
 
     public void fetchStringJsonData()
     {
-        String url="http://httpbin.org/html";
+        String url="http://api.androidhive.info/volley/person_array.json";
         StringRequest request=new StringRequest(Request.Method.GET, url, new Response.Listener<String>(){
             @Override
             public void onResponse(String response)
@@ -56,6 +62,50 @@ public class MainActivity extends AppCompatActivity {
         });
         requestQueue.add(request);
     }
+
+    public void fetchArrayJsonData()
+    {
+        jsonText.setText("Rohit");
+        String url="http://api.androidhive.info/volley/person_array.json";
+
+        JsonArrayRequest request=new JsonArrayRequest(Request.Method.GET, url,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+
+                        String allData="";
+                        for (int i = 0; i < response.length();i++)
+                        {
+                            try {
+                                JSONObject obj = response.getJSONObject(i);
+                                jsonText.setText(obj.toString());
+                                String name = obj.getString("name");
+                                String email = obj.getString("email");
+                                JSONObject phone = obj.getJSONObject("phone");
+                                String home = phone.getString("home");
+                                String mobile = phone.getString("mobile");
+                                allData = allData+"\n\n"+name + "\n" + email + "\n" + home + "\n" + mobile;
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        jsonText.setText(allData);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                }
+        );
+        requestQueue.add(request);
+
+
+    }
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -78,4 +128,5 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }
