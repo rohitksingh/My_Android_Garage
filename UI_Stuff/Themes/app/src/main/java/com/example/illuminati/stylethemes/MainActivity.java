7076c7a0d527b87a60539892bd12f95c2267145f
rohitksingh <r4rohit002@gmail.com>
2016-09-01 08:39:37 -0700

    Custom TextView Test success(Working with external .ttf file)

---
+++ UI_Stuff/Themes/app/src/main/java/com/example/illuminati/stylethemes/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.illuminati.stylethemes;
+
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView text;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
+        text=(TextView)findViewById(R.id.text);
+        Typeface customTypeFace=Typeface.createFromAsset(getResources().getAssets(),"fonts/AtlantideStarlight.ttf");
+        text.setTypeface(customTypeFace);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
