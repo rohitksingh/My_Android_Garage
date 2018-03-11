a8050e953944caf5ac82ca61dc5ff1424852208d
rohitksingh <r4rohit002@gmail.com>
2018-03-10 22:18:31 -0800

    More example added

--- NotificationDemo/app/src/main/java/rohksin/com/notificationdemo/MainActivity.java
+++ NotificationDemo/app/src/main/java/rohksin/com/notificationdemo/MainActivity.java
@@ -10,6 +10,7 @@ import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -22,6 +23,9 @@ public class MainActivity extends AppCompatActivity{
     private Button button1;
     private Button button2;
     private Button button3;
+    private Button button4;
+    private Button button5;
+    private Button button6;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,12 +33,58 @@ public class MainActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         button = (Button)findViewById(R.id.button);
+        button1 = (Button)findViewById(R.id.button1);
+        button2 = (Button)findViewById(R.id.button2);
+        button3 = (Button)findViewById(R.id.button3);
+        button4 = (Button)findViewById(R.id.button4);
+        button5 = (Button)findViewById(R.id.button5);
+        button6 = (Button)findViewById(R.id.button6);
+
+
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
 
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                sendNotidication(getBasicNotification().build());
+                sendNotification(getBasicNotification().build());
+            }
+        });
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendNotification(getAutoCanceableNotification().build());
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendNotification(getNonCancelableNotification().build());
+            }
+        });
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendNotification(getCustomNotification().build());
+            }
+        });
+
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendNotification(getLargeImageNotification().build());
+            }
+        });
+
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendNotification(getMultilineNotification().build());
             }
         });
 
@@ -71,12 +121,34 @@ public class MainActivity extends AppCompatActivity{
     {
 
         return getBasicNotification()
-                .setLargeIcon(BitmapFactory.decodeResource(MainActivity.this.getResources(), android.R.drawable.stat_sys_download))
+                .setLargeIcon(BitmapFactory.decodeResource(MainActivity.this.getResources(), R.mipmap.ic_launcher))
                 .setTicker("This is ticker message")
                 .setNumber(4);
 
     }
 
+
+    public NotificationCompat.Builder getLargeImageNotification()
+    {
+        NotificationCompat.BigPictureStyle style = new NotificationCompat.BigPictureStyle();
+        style.bigPicture(BitmapFactory.decodeResource(MainActivity.this.getResources(), R.drawable.big_pic))
+                .setBigContentTitle("Big title")
+                .setSummaryText("This is summary text This is summary text This is summary text This is summary text This is summary text This is summary text This is summary text This is summary text ");
+
+        return getBasicNotification()
+                .setStyle(style);
+    }
+
+    public NotificationCompat.Builder getMultilineNotification()
+    {
+        NotificationCompat.BigTextStyle style = new NotificationCompat.BigTextStyle();
+        style.setBigContentTitle("Big Title")
+                .setSummaryText("Summary Text")
+                .bigText("Hi this is story Hi this is story Hi this is story Hi this is story Hi this is story Hi this is story Hi this is story Hi this is story Hi this is story Hi this is story Hi this is story Hi this is story Hi this is story Hi this is story Hi this is story Hi this is story Hi this is story Hi this is story ");
+
+        return getBasicNotification()
+                .setStyle(style);
+    }
 
 
     public PendingIntent getContentIntent()
@@ -88,15 +160,11 @@ public class MainActivity extends AppCompatActivity{
 
 
 
-    public void sendNotidication(Notification notification)
+    public void sendNotification(Notification notification)
     {
         NotificationManager notificationManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
         notificationManager.notify(100, notification);
     }
-
-
-
-
 
 
 
