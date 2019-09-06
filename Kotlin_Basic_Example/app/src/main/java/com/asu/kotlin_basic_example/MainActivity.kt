001faa2e6caa76ef99ca70e0b450994442bcb6e4
Rohit <r4rohit002@gmail.com>
2019-09-06 16:04:18 -0700

    Added Button Click listeners

--- Kotlin_Basic_Example/app/src/main/java/com/asu/kotlin_basic_example/MainActivity.kt
+++ Kotlin_Basic_Example/app/src/main/java/com/asu/kotlin_basic_example/MainActivity.kt
@@ -1,27 +1,39 @@
 package com.asu.kotlin_basic_example
 
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main);
+
+        button.setOnClickListener{
+            Toast.makeText(this, "Button 1 Clicked", Toast.LENGTH_SHORT).show();
         }
+
+        button2.setOnClickListener{
+            Toast.makeText(this, "Button 2 Clicked", Toast.LENGTH_LONG).show();
+        }
+
+
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+
+    /*override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
@@ -35,5 +47,5 @@ class MainActivity : AppCompatActivity() {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
-    }
+    }*/
 }
