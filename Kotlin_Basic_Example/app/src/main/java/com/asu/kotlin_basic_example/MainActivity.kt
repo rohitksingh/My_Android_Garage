5a19768ffb750b88e55540a15af5a74008448131
Rohit <r4rohit002@gmail.com>
2019-09-06 16:03:39 -0700

    Removed unncessary files

--- Kotlin_Basic_Example/app/src/main/java/com/asu/kotlin_basic_example/MainActivity.kt
+++ Kotlin_Basic_Example/app/src/main/java/com/asu/kotlin_basic_example/MainActivity.kt
@@ -17,7 +17,7 @@ class MainActivity : AppCompatActivity() {
 
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+                .setAction("Action", null).show()
         }
     }
 
@@ -31,7 +31,7 @@ class MainActivity : AppCompatActivity() {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        return when(item.itemId) {
+        return when (item.itemId) {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
