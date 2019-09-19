307f35e8de7f70c71ddf80a3749efaa6b3b17c10
Rohit <r4rohit002@gmail.com>
2019-09-18 19:36:21 -0700

    added Explanation and relevant url

--- NewFirebaseTutorial/app/src/main/java/com/asu/firebasetutorial/CloudStoreActivity.java
+++ NewFirebaseTutorial/app/src/main/java/com/asu/firebasetutorial/CloudStoreActivity.java
@@ -14,10 +14,34 @@ import com.google.firebase.firestore.FirebaseFirestore;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+/*************************************************************************************************
+ *
+ *       Adding an entry in Firebase CloudStorage
+ *       1) FirebaseCloudStorage is different from FirebaseRealTime Database
+ *       2) Here I am using Firebase CloudStorage
+ *
+ *          How to get Started?
+ *          Connect to Firebase Console -> Use Android Studio for this, Its easy
+ *          Add dependency for FireStorage  -> eg: implementation 'com.google.firebase:firebase-firestore:21.1.1'
+ *          It has 2 main classes
+ *              FirebaseFirestore   - Which is the main Repository
+ *              CollectionReference - Which is child repository
+ *
+ *              Adding data to Firebase CloudStorage -> addProfile()
+ *              Reading list of Data
+ *
+ *
+ *             https://github.com/rohitksingh/FBN-Admin/blob/master/app/src/main/java/admin/fbn/com/fbnadmin/AppUtility.java to see add and update
+ *
+ *
+ ************************************************************************************************/
+
+
 public class CloudStoreActivity extends AppCompatActivity implements View.OnClickListener {
 
     private EditText name, address;
-    private Button submit;
+    private Button submit, read;
 
     private static final String TAG = "CloudStoreActivity";
 
@@ -33,30 +57,21 @@ public class CloudStoreActivity extends AppCompatActivity implements View.OnClic
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cloudstore);
-
-        initFirebase();
-
-        name = findViewById(R.id.name);
-        address = findViewById(R.id.address);
-        submit = findViewById(R.id.submit);
-        submit.setOnClickListener(this);
+        initFirebaseCloudStorage();
+        setUpUI();
 
     }
 
-    private static void initFirebase(){
-
-        mainDatabase = FirebaseFirestore.getInstance();
-        profileDatabase = mainDatabase.collection(PROFILE_DATABASE_NAME);
-
-    }
-
+    /***********************************************************************************************
+     *                                  Interface methods
+     ***********************************************************************************************/
     @Override
     public void onClick(View v) {
 
         switch (v.getId()){
 
             case R.id.submit:
-                savetoserver();
+                addToServer();
                 break;
 
             default:
@@ -66,11 +81,61 @@ public class CloudStoreActivity extends AppCompatActivity implements View.OnClic
 
     }
 
-    private void savetoserver(){
 
-        Toast.makeText(this, "Saved to DB", Toast.LENGTH_SHORT).show();
-        addProduct(getPeople());
+    /***********************************************************************************************
+     *                              Init, Write, Read methods
+     ***********************************************************************************************/
 
+    private static void initFirebaseCloudStorage(){
+
+        mainDatabase = FirebaseFirestore.getInstance();
+        profileDatabase = mainDatabase.collection(PROFILE_DATABASE_NAME);
+
+    }
+
+    private void addPeople(People people){
+
+        profileDatabase
+                .document(people.getUID())
+                .set(people)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Toast.makeText(CloudStoreActivity.this, "Added to Server", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(Exception e) {
+                        Log.d(TAG, "onFailure: ");
+                        Toast.makeText(CloudStoreActivity.this, "Failed", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+    }
+
+    private void getAllPeople(){
+
+    }
+
+
+
+    /***********************************************************************************************
+     *                                       Private methods
+     ***********************************************************************************************/
+
+    private void setUpUI(){
+        name = findViewById(R.id.name);
+        address = findViewById(R.id.address);
+        submit = findViewById(R.id.submit);
+        submit.setOnClickListener(this);
+        read = findViewById(R.id.read);
+        read.setOnClickListener(this);
+
+    }
+
+    private void addToServer(){
+        addPeople(getPeople());
     }
 
     private People getPeople(){
@@ -82,24 +147,7 @@ public class CloudStoreActivity extends AppCompatActivity implements View.OnClic
 
     }
 
-    public static void addProduct(People people){
+    // Adding a entry to CloudStorage
 
-        profileDatabase
-                .document(people.getUID())
-                .set(people)
-                .addOnSuccessListener(new OnSuccessListener<Void>() {
-                    @Override
-                    public void onSuccess(Void aVoid) {
-                        Log.d(TAG, "onSuccess: ");
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(Exception e) {
-                        Log.d(TAG, "onFailure: ");
-                    }
-                });
-
-    }
 
 }
