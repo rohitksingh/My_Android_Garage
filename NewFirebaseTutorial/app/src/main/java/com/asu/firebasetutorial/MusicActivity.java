a48b81808380e6046e655e21888c361e6aebcddf
Rohit <r4rohit002@gmail.com>
2019-09-18 20:53:23 -0700

    Tested RealTime Sync in a mp3 File

--- NewFirebaseTutorial/app/src/main/java/com/asu/firebasetutorial/MusicActivity.java
+++ NewFirebaseTutorial/app/src/main/java/com/asu/firebasetutorial/MusicActivity.java
@@ -2,16 +2,34 @@ package com.asu.firebasetutorial;
 
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TabHost;
 
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.ListenerRegistration;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MusicActivity extends AppCompatActivity implements View.OnClickListener {
+public class MusicActivity extends AppCompatActivity implements View.OnClickListener, EventListener<QuerySnapshot> {
+
+    private static final String TAG = "MusicActivity";
 
     private Button pause, play;
     MediaPlayer song;
+    private final String PLAY_SONG = "MusicActivity.PLAY_SONG";
+    private final String PAUSE_SONG = "MusicActivity.PAUSE_SONG";
+
+
+    FirebaseFirestore maindatabase;
+    CollectionReference songRepository;
+    private ListenerRegistration registration;
 
     @Override
     public void onCreate(Bundle savedInstanceState){
@@ -23,7 +41,22 @@ public class MusicActivity extends AppCompatActivity implements View.OnClickList
         play = findViewById(R.id.pause);
         pause.setOnClickListener(this);
         play.setOnClickListener(this);
+        initFirebaseCloudStorage();
 
+        registration = songRepository.addSnapshotListener(this);
+
+    }
+
+    private void initFirebaseCloudStorage(){
+
+        maindatabase = FirebaseFirestore.getInstance();
+        songRepository = maindatabase.collection("Song");
+    }
+
+    private void changeStatus(String status){
+        Log.d(TAG, "changeStatus: "+status);
+        songRepository.document("song")
+                .set(new Song(status));
     }
 
     private void playSong(){
@@ -42,15 +75,43 @@ public class MusicActivity extends AppCompatActivity implements View.OnClickList
         switch (v.getId()){
 
             case R.id.play:
-                playSong();
+                //playSong();
+                changeStatus(PLAY_SONG);
                 break;
 
             case R.id.pause:
-                pauseSong();
+                //pauseSong();
+                changeStatus(PAUSE_SONG);
                 break;
 
             default:
                 break;
+
+        }
+    }
+
+
+    @Override
+    public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e) {
+
+        for(DocumentChange dc : queryDocumentSnapshots.getDocumentChanges()){
+
+            if(dc.getType().toString().equals("MODIFIED")) {
+
+                String state = dc.getDocument().getString("state");
+
+                if (state.equals(PLAY_SONG)) {
+
+                    playSong();
+
+                } else if (state.equals(PAUSE_SONG)){
+
+                    pauseSong();
+
+                }
+            }
+            Log.d(TAG, "onEvent: "+dc.getType().toString()+" "+dc.getDocument().getData());
+
 
         }
     }
---
+++ NewFirebaseTutorial/app/src/main/java/com/asu/firebasetutorial/Song.java
@@ -0,0 +1,18 @@
+package com.asu.firebasetutorial;
+
+public class Song {
+
+    public String state;
+
+    public Song(String state){
+        this.state = state;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+}
