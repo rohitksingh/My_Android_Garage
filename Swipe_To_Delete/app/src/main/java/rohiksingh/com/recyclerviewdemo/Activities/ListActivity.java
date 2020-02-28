be5e0952af5a4a43dfb8faccc8b273a2ac55caec
Rohit <r4rohit002@gmail.com>
2020-02-27 23:27:56 -0700

    Initial Commit for swipe to delete

---
+++ Swipe_To_Delete/app/src/main/java/rohiksingh/com/recyclerviewdemo/Activities/ListActivity.java
@@ -0,0 +1,60 @@
+package rohiksingh.com.recyclerviewdemo.Activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import rohiksingh.com.recyclerviewdemo.Adapters.PeopleListAdapter;
+import rohiksingh.com.recyclerviewdemo.Callbacks.ListCallback;
+import rohiksingh.com.recyclerviewdemo.Models.Person;
+import rohiksingh.com.recyclerviewdemo.R;
+import rohiksingh.com.recyclerviewdemo.Utilities.AppUtility;
+
+public class ListActivity extends AppCompatActivity implements ListCallback{
+
+    private RecyclerView rv;
+    private RecyclerView.LayoutManager layoutManager;
+    private RecyclerView.Adapter adapter;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+        rv = (RecyclerView)findViewById(R.id.rv);
+        adapter = new PeopleListAdapter(this, getDummyList());
+        layoutManager = new LinearLayoutManager(this);
+        rv.setLayoutManager(layoutManager);
+        rv.setAdapter(adapter);
+    }
+
+
+
+    private List<Person> getDummyList(){
+
+        List<Person> people = new ArrayList<Person>();
+
+        for(int i=0;i<20;i++){
+            Person person = new Person();
+            person.setName("Name "+i);
+            people.add(person);
+        }
+
+        return people;
+    }
+
+    private void openDetail(Person people){
+        Intent intent = new Intent(this, DetailActivity.class);
+        intent.putExtra(AppUtility.NAME, people);
+        startActivity(intent);
+    }
+
+    @Override
+    public void click(Person people) {
+        openDetail(people);
+    }
+}
---
+++ Swipe_To_Delete/app/src/main/java/rohiksingh/com/recyclerviewdemo/Utilities/AppUtility.java
@@ -0,0 +1,7 @@
+package rohiksingh.com.recyclerviewdemo.Utilities;
+
+public class AppUtility {
+
+    public static String NAME = "rohiksingh.com.recyclerviewdemo.Utilities.name";
+
+}
