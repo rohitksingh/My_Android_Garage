1913d2dbc0bbefc49e5366fd5791feb154af102e
Rohit <r4rohit002@gmail.com>
2020-03-01 17:18:39 -0700

    Code cleanup

--- AutoCompleteTextView/app/src/androidTest/java/com/rohitksingh/autocompetetextviewdemo/ExampleInstrumentedTest.java
+++ AutoCompleteTextView/app/src/androidTest/java/com/rohitksingh/autocompetetextviewdemo/ExampleInstrumentedTest.java
@@ -1,6 +1,7 @@
 package com.rohitksingh.autocompetetextviewdemo;
 
 import android.content.Context;
+
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
