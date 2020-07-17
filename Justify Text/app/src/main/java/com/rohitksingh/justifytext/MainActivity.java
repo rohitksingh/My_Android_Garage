42b43543a983d2ae76f2e2bce2c399969b82729d
Rohit <r4rohit002@gmail.com>
2020-07-16 21:29:26 -0700

    Added justified text

--- Justify Text/app/src/main/java/com/rohitksingh/justifytext/MainActivity.java
+++ Justify Text/app/src/main/java/com/rohitksingh/justifytext/MainActivity.java
@@ -1,15 +1,22 @@
 package com.rohitksingh.justifytext;
 
 import android.os.Bundle;
+import android.webkit.WebView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private WebView webView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        webView = findViewById(R.id.justifyText);
+        webView.loadData(getString(R.string.about_boole), "text/html; charset=utf-8", "utf-8");
+        webView.setBackgroundColor(0x00000000);
+
     }
 
 }
