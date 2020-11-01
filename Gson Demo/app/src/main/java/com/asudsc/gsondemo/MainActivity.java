fe79263e15d53be5627e25670e55fe1b5cec5878
Rohit <r4rohit002@gmail.com>
2020-10-31 19:17:24 -0700

    Initial commit

---
+++ Gson Demo/app/src/main/java/com/asudsc/gsondemo/MainActivity.java
@@ -0,0 +1,53 @@
+package com.asudsc.gsondemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.graphics.drawable.Icon;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        parseNormalJson();
+    }
+
+
+    public void parseNormalJson(){
+
+        String jsonResponse = readJsonFromFile(this, "simple.json");
+
+    }
+
+
+
+    public static String readJsonFromFile(Context context, String fileName){
+
+        String json = null;
+        try {
+            InputStream is = context.getAssets().open(fileName);
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, StandardCharsets.UTF_8);
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+        return json;
+    }
+
+
+}
---
+++ Gson Demo/app/src/main/java/com/asudsc/gsondemo/User.java
@@ -0,0 +1,41 @@
+package com.asudsc.gsondemo;
+
+public class User {
+
+    private String name;
+    private int age;
+    private String email;
+    private boolean isDeveloper;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isDeveloper() {
+        return isDeveloper;
+    }
+
+    public void setDeveloper(boolean developer) {
+        isDeveloper = developer;
+    }
+}
