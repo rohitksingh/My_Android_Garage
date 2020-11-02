5e0874d746984ae2295009d457f4924f3811e70d
Rohit <r4rohit002@gmail.com>
2020-11-02 13:39:15 -0700

    Added Retrofit2 Dependency and Created RESTClient for API

---
+++ Retrofit2 Demo/app/src/main/java/com/asudsc/retrofit2demo/PlaceHolderClient.java
@@ -0,0 +1,13 @@
+package com.asudsc.retrofit2demo;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface PlaceHolderClient {
+
+    @GET(/posts)
+    Call<List<Post>> getAllPosts();
+
+}
---
+++ Retrofit2 Demo/app/src/main/java/com/asudsc/retrofit2demo/Post.java
@@ -0,0 +1,8 @@
+package com.asudsc.retrofit2demo;
+
+public class Post {
+    public int userId;
+    public int id;
+    public String title;
+    public String body;
+}
