bfd33b3c064a99a0222b84bf31b9e4abf2a321e4
Rohit <r4rohit002@gmail.com>
2020-11-02 14:21:54 -0700

    Changed POJO filed name and mapped the field with JSON field using @SerializedName

--- Retrofit2 Demo/app/src/main/java/com/asudsc/retrofit2demo/Post.java
+++ Retrofit2 Demo/app/src/main/java/com/asudsc/retrofit2demo/Post.java
@@ -1,12 +1,16 @@
 package com.asudsc.retrofit2demo;
 
+import com.google.gson.annotations.SerializedName;
+
 public class Post {
-    public int userId;
+
+    @SerializedName("userId")
+    public int user_id;
     public int id;
     public String title;
     public String body;
 
     public String toString(){
-        return userId+" "+id+" "+title+" "+body;
+        return user_id+" "+id+" "+title+" "+body;
     }
 }
