598c95704ab4f1c51be261f19e1028aadf04379f
Rohit <r4rohit002@gmail.com>
2021-01-31 02:47:58 -0700

    Added Service Generator and RequestAPI class

--- RXJava/app/src/main/java/com/rohitksingh/rxjavademo/flatmap/requests/RequestAPI.java
+++ RXJava/app/src/main/java/com/rohitksingh/rxjavademo/flatmap/requests/RequestAPI.java
@@ -1,4 +1,19 @@
 package com.rohitksingh.rxjavademo.flatmap.requests;
 
+import com.rohitksingh.rxjavademo.flatmap.models.Comment;
+import com.rohitksingh.rxjavademo.flatmap.models.Post;
+
+import java.util.List;
+
+import io.reactivex.rxjava3.core.Observable;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
 public interface RequestAPI {
+
+    @GET("posts")
+    Observable<List<Post>> getAllPost();
+
+    @GET("post/{id}/comments")
+    Observable<List<Comment>> getAllComments(@Path("id") int id);
 }
--- RXJava/app/src/main/java/com/rohitksingh/rxjavademo/flatmap/requests/ServiceGenerator.java
+++ RXJava/app/src/main/java/com/rohitksingh/rxjavademo/flatmap/requests/ServiceGenerator.java
@@ -1,4 +1,25 @@
 package com.rohitksingh.rxjavademo.flatmap.requests;
 
+import hu.akarnokd.rxjava3.retrofit.RxJava3CallAdapterFactory;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class ServiceGenerator {
+
+    public static final String BASE_URL = "";
+
+    private static Retrofit.Builder retrofitBuilder = new Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create());
+
+    private static Retrofit retrofit = retrofitBuilder.build();
+
+    private static RequestAPI requestAPI = retrofit.create(RequestAPI.class);
+
+    public static RequestAPI getRequestAPI(){
+        return requestAPI;
+    }
+
+
 }
