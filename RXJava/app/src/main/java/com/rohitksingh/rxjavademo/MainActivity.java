bea9047a91a3672499929d1cc6ffbe9afa0b3e23
Rohit <r4rohit002@gmail.com>
2021-01-31 00:59:40 -0700

    Initial commit

---
+++ RXJava/app/src/main/java/com/rohitksingh/rxjavademo/MainActivity.java
@@ -0,0 +1,24 @@
+package com.rohitksingh.rxjavademo;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
