414a8921cdfdd2b9998533f6cde4db8878601453
Rohit <r4rohit002@gmail.com>
2021-01-31 15:03:27 -0700

    Initialised RV and cleared discomposable in onDestroy

--- RXJava/app/src/main/java/com/rohitksingh/rxjavademo/flatmap/PostListActivity.java
+++ RXJava/app/src/main/java/com/rohitksingh/rxjavademo/flatmap/PostListActivity.java
@@ -6,16 +6,35 @@ import com.rohitksingh.rxjavademo.R;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import io.reactivex.rxjava3.disposables.CompositeDisposable;
 
 public class PostListActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
+    private CompositeDisposable compositeDisposable;
+    private RVAdapter adapter;
+    private LinearLayoutManager llm;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_list);
         recyclerView = findViewById(R.id.rv);
+        initRV();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        compositeDisposable.clear();
+    }
+
+    private void initRV(){
+        adapter = new RVAdapter();
+        llm = new LinearLayoutManager(this);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(llm);
     }
 }
