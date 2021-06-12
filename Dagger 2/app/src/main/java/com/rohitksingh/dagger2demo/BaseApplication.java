d1cb1f4a7f0ad7e353440167358dc9973df1b51f
Rohit <r4rohit002@gmail.com>
2021-06-11 23:33:12 -0700

    Created AppComponent

--- Dagger 2/app/src/main/java/com/rohitksingh/dagger2demo/BaseApplication.java
+++ Dagger 2/app/src/main/java/com/rohitksingh/dagger2demo/BaseApplication.java
@@ -1,11 +1,14 @@
 package com.rohitksingh.dagger2demo;
 
+import com.rohitksingh.dagger2demo.di.AppComponent;
+import com.rohitksingh.dagger2demo.di.DaggerAppComponent;
+
 import dagger.android.AndroidInjector;
 import dagger.android.support.DaggerApplication;
 
 public class BaseApplication extends DaggerApplication {
     @Override
     protected AndroidInjector<? extends DaggerApplication> applicationInjector() {
-        return null;
+        return DaggerAppComponent.builder().application(this).build();
     }
 }
---
+++ Dagger 2/app/src/main/java/com/rohitksingh/dagger2demo/di/AppComponent.java
@@ -0,0 +1,28 @@
+package com.rohitksingh.dagger2demo.di;
+
+import android.app.Application;
+
+import com.rohitksingh.dagger2demo.BaseApplication;
+
+import dagger.BindsInstance;
+import dagger.Component;
+import dagger.android.AndroidInjector;
+import dagger.android.support.AndroidSupportInjectionModule;
+
+@Component(
+        modules = { AndroidSupportInjectionModule.class }
+)
+public interface AppComponent extends AndroidInjector<BaseApplication> {
+
+    @Component.Builder
+    interface Builder{
+
+        @BindsInstance
+        Builder application(Application application);
+
+        AppComponent build();
+
+    }
+
+
+}
