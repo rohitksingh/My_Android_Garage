9f59d996689b988d4f95e931bf457ba7ba544aeb
Rohit <r4rohit002@gmail.com>
2021-06-12 21:25:31 -0700

    Added ViewModelFactoryModule

---
+++ Dagger 2/app/src/main/java/com/rohitksingh/dagger2demo/di/ViewModelFactoryModule.java
@@ -0,0 +1,15 @@
+package com.rohitksingh.dagger2demo.di;
+
+import com.rohitksingh.dagger2demo.viewmodels.ViewModelProviderFactory;
+
+import androidx.lifecycle.ViewModelProvider;
+import dagger.Binds;
+import dagger.Module;
+
+@Module
+public abstract class ViewModelFactoryModule {
+
+    @Binds
+    public abstract ViewModelProvider.Factory bindViewModelFactory(ViewModelProviderFactory viewModelProviderFactory);
+
+}
--- Dagger 2/app/src/main/java/com/rohitksingh/dagger2demo/viewmodels/AuthViewModel.java
+++ Dagger 2/app/src/main/java/com/rohitksingh/dagger2demo/viewmodels/AuthViewModel.java
@@ -2,12 +2,16 @@ package com.rohitksingh.dagger2demo.viewmodels;
 
 import android.util.Log;
 
+import javax.inject.Inject;
+
 import androidx.lifecycle.ViewModel;
 
+//You can not pass parameter in Constructor. That is why using Constructor Injection
 public class AuthViewModel extends ViewModel {
 
     private static final String TAG = "AuthViewModel";
 
+    @Inject
     public AuthViewModel(){
 
         Log.d(TAG, "AuthViewModel: created");
